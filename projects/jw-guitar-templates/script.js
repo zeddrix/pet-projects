8201fd02baecf5bc247feb60ae060bcbe81a7c9c
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2020-03-25 15:44:26 +0800

    Alinged the elements properly. Filtered for the song searched for.

--- projects/jw-guitar-templates/script.js
+++ projects/jw-guitar-templates/script.js
@@ -2,13 +2,39 @@ const songs = [{
     number: 78,
     title: "\"Teaching the Word of God\"",
     verse: "Acts 18:11",
+},
+{
+    number: 79,
+    title: "Teach Them to Stand Firm",
+    verse: "Matthew 28:19, 20",
+},
+{
+    number: 80,
+    title: "\"Taste and See That Jehovah is Good\"",
+    verse: "Psalm 34:8",
 }];
 
+
+const songToFind = "78";
+
+
+const result = songs.filter(song => song.number.toString(10) === songToFind);
+
+/*
+result = [
+{
+    number: 79,
+    title: "Teach Them to Stand Firm",
+    verse: "Matthew 28:19, 20",
+}];
+*/
+
 let songNumber = document.getElementById("songNumber");
-songNumber.innerText = songs[0].number;
+songNumber.innerText = result[0].number;
 
 let songTitle = document.getElementById("songTitle");
-songTitle.innerText = songs[0].title
+songTitle.innerText = result[0].title;
 
 let bibleVerse = document.getElementById("bibleVerse");
-bibleVerse.innerText = songs[0].verse
+bibleVerse.innerText = result[0].verse;
+
