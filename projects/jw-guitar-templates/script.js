e332b1d5ee4d72c08b1d5d3e2facfb349ae1b293
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2020-03-25 12:36:17 +0800

    Able to get the data from the array object.

--- projects/jw-guitar-templates/script.js
+++ projects/jw-guitar-templates/script.js
@@ -1,1 +1,14 @@
-const songNo = document.getElementById("songNumber");
+const songs = [{
+    number: 78,
+    title: "\"Teaching the Word of God\"",
+    verse: "Acts 18:11",
+}];
+
+let songNumber = document.getElementById("songNumber");
+songNumber.innerText = songs[0].number;
+
+let songTitle = document.getElementById("songTitle");
+songTitle.innerText = songs[0].title
+
+let bibleVerse = document.getElementById("bibleVerse");
+bibleVerse.innerText = songs[0].verse
