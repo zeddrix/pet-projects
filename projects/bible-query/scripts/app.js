124a94aa7f3523b3b7030a313eeac51b4090577e
zeddrix <37854513+zeddrix@users.noreply.github.com>
2020-05-09 09:01:04 +0800

    renamed Q&A to quiz.

--- projects/bible-query/scripts/app.js
+++ projects/bible-query/scripts/app.js
@@ -6,3 +6,12 @@ const rightAnswer = () => {
 const wrongAnswer = () => {
     console.log('SORRY, WRONG ANSWER.')
 }
+
+// for (let c = 0; c < arrayLength; c++) {
+//     const button = document.createElement("button");
+
+//     button.innerText = colors[c].label;
+//     button.classList.add(colors[c].name, "button-prop");
+//     button.addEventListener('click', () => changeColor(colors[c].colorValue));
+//     parent.prepend(button);
+// }
