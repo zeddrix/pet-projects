21b97e4f5996e2b806e760c55595d5e00c31ce7e
zeddrix <37854513+zeddrix@users.noreply.github.com>
2020-05-11 09:15:34 +0800

    gitignore

--- projects/bible-query/scripts/app.js
+++ projects/bible-query/scripts/app.js
@@ -6,11 +6,12 @@ const rightAnswer = () => {
 const wrongAnswer = () => {
     console.log('SORRY, WRONG ANSWER.')
 }
+console.log(men[0].choices[3]);
 
 // for (let c = 0; c < arrayLength; c++) {
-//     const button = document.createElement("button");
-
-//     button.innerText = colors[c].label;
+    const button = document.createElement("button");
+    // button.innerText = 
+    // parent.prepend(button);
 //     button.classList.add(colors[c].name, "button-prop");
 //     button.addEventListener('click', () => changeColor(colors[c].colorValue));
 //     parent.prepend(button);
