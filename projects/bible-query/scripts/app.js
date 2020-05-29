b89812ed7e6a437f4e319358fe421af0bd9ca233
zeddrix <37854513+zeddrix@users.noreply.github.com>
2020-05-29 09:23:23 +0800

    Added modal on tell-your-friends.

--- projects/bible-query/scripts/app.js
+++ projects/bible-query/scripts/app.js
@@ -4,6 +4,7 @@ const finalScoreBlock = document.getElementById("final-score-block");
 const settingsBlock = document.getElementById("settings-block");
 const rulesBlock = document.getElementById("rules-block");
 const aboutBlock = document.getElementById("about-block");
+const tellYourFriendsBtn = document.getElementById("tell-your-friends-btn");
 
 const queryBlock = document.getElementById("query-block");
 const question = document.getElementById("question");
@@ -31,6 +32,28 @@ let count = 0;
 let Timer;
 let score = 0;
 
+// Modal
+const modal = document.getElementById("tell-your-friends-modal");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+tellYourFriendsBtn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
 const bibleQueryHomepage = () => {
   homepage.style.display = "block";
   levelsBlock.style.display = "none";
