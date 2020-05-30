eba96eaf2f107801c613e32885002cc6718a9ae1
zeddrix <37854513+zeddrix@users.noreply.github.com>
2020-05-30 15:56:07 +0800

    Inputed data for 'women'.

--- projects/bible-query/scripts/query.js
+++ projects/bible-query/scripts/query.js
@@ -1,4 +1,4 @@
-let men = [
+const men = [
   // {
   //     question : ,
   //     clue : ,
@@ -242,5 +242,239 @@ let men = [
     choiceC: "Joshua",
     choiceD: "Samson",
     correctAnswer: "D",
+  },
+];
+
+const women = [
+  {
+    question: "What is the name of Isaac’s wife?",
+    clueSource: "Genesis 24:67",
+    clueSourceContent:
+      "After that Isaac brought her into the tent of Sarah his mother. Thus he took Rebekah as his wife; and he fell in love with her, and Isaac found comfort after the loss of his mother.",
+    choiceA: "Rachel",
+    choiceB: "Rebekah",
+    choiceC: "Sarah",
+    choiceD: "Leah",
+    correctAnswer: "B",
+  },
+  {
+    question:
+      "She is one of Jacob’s wives and her sons were Joseph and Benjamin.",
+    clueSource: "Genesis 35:24",
+    clueSourceContent: "The sons by Rachel were Joseph and Benjamin.",
+    choiceA: "Leah",
+    choiceB: "Bilhah",
+    choiceC: "Zilpah",
+    choiceD: "Rachel",
+    correctAnswer: "D",
+  },
+  {
+    question: "Who became Boaz’s wife?",
+    clueSource: "Ruth 4:13",
+    clueSourceContent:
+      "So Boaz took Ruth and she became his wife. He had relations with her, and Jehovah let her conceive and she gave birth to a son.",
+    choiceA: "Naomi",
+    choiceB: "Ruth",
+    choiceC: "Sarah",
+    choiceD: "Orpah",
+    correctAnswer: "B",
+  },
+  {
+    question:
+      "Her son, Solomon, replaced her husband David to rule as king in Israel.",
+    clueSource: "1 Kings 1:11",
+    clueSourceContent:
+      "Nathan then said to Bathsheba, Solomon’s mother: “Have you not heard that Adonijah the son of Haggith has become king, and our lord David does not know anything about it?”",
+    choiceA: "Ahinoam",
+    choiceB: "Abigail",
+    choiceC: "Bath-sheba",
+    choiceD: "Michal",
+    correctAnswer: "C",
+  },
+  {
+    question:
+      "Who said this to Jesus: “Lord, does it not matter to you that my sister has left me alone to attend to things? Tell her to come and help me.”",
+    clueSource: "Luke 10:40",
+    clueSourceContent:
+      "Martha, on the other hand, was distracted with attending to many duties. So she came to him and said: “Lord, does it not matter to you that my sister has left me alone to attend to things? Tell her to come and help me.”",
+    choiceA: "Mary",
+    choiceB: "The Samaritan Woman",
+    choiceC: "Martha",
+    choiceD: "Leah",
+    correctAnswer: "C",
+  },
+  {
+    question: "She was mistreated by Peninnah for being barren.",
+    clueSource: "1 Samuel 1:4-6",
+    clueSourceContent:
+      "4 One day when Elkanah offered a sacrifice, he gave portions to his wife Peninnah as well as to all her sons and her daughters, 5 but to Hanʹnah he gave a special portion, because Hanʹnah was the one he loved; but Jehovah had not given her children. 6 Moreover, her rival wife taunted her relentlessly in order to upset her because Jehovah had not given her children.",
+    choiceA: "Dinah",
+    choiceB: "Hannah",
+    choiceC: "Penninah",
+    choiceD: "Elkanah",
+    correctAnswer: "B",
+  },
+  {
+    question:
+      "Her house, which was on the wall, remained standing after the fall of the walls of Jericho.",
+    clueSource: "Joshua 6:23",
+    clueSourceContent:
+      "So the young spies went in and brought out Rahab, along with her father, her mother, her brothers, and all who belonged to her; yes, they brought out her whole family, and they brought them safely to a place outside the camp of Israel.",
+    choiceA: "Deborah",
+    choiceB: "Rahab",
+    choiceC: "Jael",
+    choiceD: "Jezebel",
+    correctAnswer: "B",
+  },
+  {
+    question: "She is the mother-in-law of Ruth.",
+    clueSource: "Ruth 3:1",
+    clueSourceContent:
+      "Naomi, her mother-in-law, now said to her: “My daughter, should I not look for a home for you, so that it may go well with you?”",
+    choiceA: "Orpah",
+    choiceB: "Sarah",
+    choiceC: "Naomi",
+    choiceD: "Ruth",
+    correctAnswer: "C",
+  },
+  {
+    question: "She got her name from her husband.",
+    clueSource: "Genesis 3:20",
+    clueSourceContent:
+      "After this Adam named his wife Eve, because she was to become the mother of everyone living.",
+    choiceA: "Eve",
+    choiceB: "Sarah",
+    choiceC: "Priscilla",
+    choiceD: "Hannah",
+    correctAnswer: "A",
+  },
+  {
+    question: "She is Rebekah’s mother-in-law. She is also Lot’s aunt.",
+    clueSource: "Genesis 24:67",
+    clueSourceContent:
+      "After that Isaac brought her into the tent of Sarah his mother. Thus he took Rebekah as his wife; and he fell in love with her, and Isaac found comfort after the loss of his mother.",
+    choiceA: "Rachel",
+    choiceB: "Leah",
+    choiceC: "Sarah",
+    choiceD: "Zilpah",
+    correctAnswer: "C",
+  },
+  {
+    question:
+      "She is a prophetess who joined Barak in a war with the Canaanites.",
+    clueSource: "Judges 4:9",
+    clueSourceContent:
+      "To this she said: “I will certainly go with you. However, the campaign you are going on will not bring you glory, for it will be into the hand of a woman that Jehovah will give Sisera.” Then Deborah got up and went with Barak to Kedesh.",
+    choiceA: "Dinah",
+    choiceB: "Jael",
+    choiceC: "Deborah",
+    choiceD: "Rahab",
+    correctAnswer: "C",
+  },
+  {
+    question:
+      "She is a seller of purple from the city of Thyatira, and paid attention to the things Paul was saying.",
+    clueSource: "Acts 16:14",
+    clueSourceContent:
+      "And a woman named Lydia, a seller of purple from the city of Thyatira and a worshipper of God, was listening, and Jehovah opened her heart wide to pay attention to the things Paul was saying.",
+    choiceA: "Priscilla",
+    choiceB: "Lois",
+    choiceC: "Eunice",
+    choiceD: "Lydia",
+    correctAnswer: "D",
+  },
+  {
+    question:
+      "She went to Egypt with her husband because Herod wants to kill their son.",
+    clueSource: "Matthew 2:13, 14",
+    clueSourceContent:
+      "13 After they had departed, look! Jehovah’s angel appeared to Joseph in a dream, saying: “Get up, take the young child and his mother and flee to Egypt, and stay there until I give you word, for Herod is about to search for the young child to kill him.” 14 So Joseph got up and by night took along the young child and the child’s mother and went into Egypt.",
+    choiceA: "Sarah",
+    choiceB: "Rebekah",
+    choiceC: "Mary",
+    choiceD: "Rachel",
+    correctAnswer: "C",
+  },
+  {
+    question: "Her priestly father Jethro gave her in marriage to Moses.",
+    clueSource: "Exodus 2:21",
+    clueSourceContent:
+      "After that Moses consented to stay with the man, and he gave his daughter Zipporah to Moses in marriage.",
+    choiceA: "Miriam",
+    choiceB: "Zipporah",
+    choiceC: "Gershom",
+    choiceD: "Deborah",
+    correctAnswer: "B",
+  },
+  {
+    question: "Jehovah used apostle Peter to raise her up from the dead.",
+    clueSource: "Acts 9:39, 40",
+    clueSourceContent:
+      "39 At that Peter got up and went with them. And when he arrived, they led him up into the upper room; and all the widows presented themselves to him, weeping and showing many garments and robes that Dorcas had made while she was with them. 40 Peter then put everyone outside, and kneeling down, he prayed. Then turning toward the body, he said: “Tabitha, rise!” She opened her eyes, and as she caught sight of Peter, she sat up.",
+    choiceA: "Tabitha (Dorcas)",
+    choiceB: "Lois",
+    choiceC: "Eunice",
+    choiceD: "Martha",
+    correctAnswer: "A",
+  },
+  {
+    question:
+      "Her first husband was harsh and behaved badly. But her second husband was a good king.",
+    clueSource: "1 Samuel 25:39",
+    clueSourceContent:
+      "When David heard that Nabal had died, he said: “Praise Jehovah, who has pleaded my case of reproach from Nabal and has kept his servant from doing anything bad, and Jehovah has brought the badness of Nabal back on his own head!” Then David sent word to propose to Abigail to take her as his wife.",
+    choiceA: "Michal",
+    choiceB: "Ahinoam",
+    choiceC: "Abigail",
+    choiceD: "Delilah",
+    correctAnswer: "C",
+  },
+  {
+    question:
+      "She replaced Persian Queen Vashti. She is also known as “Hadassah”.",
+    clueSource: "Esther 2:7",
+    clueSourceContent:
+      "He was the guardian of Hadassah, that is, Esther, the daughter of his father’s brother, for she had neither father nor mother. The young woman was beautifully formed and attractive in appearance, and at the death of her father and her mother, Mordecai took her as his daughter.",
+    choiceA: "Hagar",
+    choiceB: "Esther",
+    choiceC: "Jezebel",
+    choiceD: "Rahab",
+    correctAnswer: "B",
+  },
+  {
+    question:
+      "Who plotted against Naboth and got him stoned to death to get his vineyard for her husband, Ahab?",
+    clueSource: "1 Kings 21:7-10",
+    clueSourceContent:
+      "His wife Jezebel said to him: “Are you not the one ruling as king over Israel? Get up, eat something, and let your heart be cheerful. I will give you the vineyard of Naboth the Jezreelite.” 8 So she wrote letters in Ahab’s name and sealed them with his seal and sent the letters to the elders and the nobles who lived in Naboth’s city. 9 She wrote in the letters: “Proclaim a fast, and have Naboth sit at the head of the people. 10 And have two good-for-nothing men sit in front of him and testify against him, saying, ‘You have cursed God and the king!’ Then bring him out and stone him to death.”",
+    choiceA: "Jehoiada",
+    choiceB: "Jezebel",
+    choiceC: "Athalia",
+    choiceD: "Esther",
+    correctAnswer: "B",
+  },
+  {
+    question:
+      "Jehovah God struck her with leprosy for speaking against Moses because of the Cushite wife he had married.",
+    clueSource: "Numbers 12:10",
+    clueSourceContent:
+      "The cloud moved away from over the tent, and look! Miriam was struck with leprosy as white as snow. Then Aaron turned toward Miriam, and he saw that she was struck with leprosy.",
+    choiceA: "Hannah",
+    choiceB: "Miriam",
+    choiceC: "The daughter of Pharaoh",
+    choiceD: "Jokebed",
+    correctAnswer: "B",
+  },
+  {
+    question:
+      "She is Jacob’s daughter by Leah who used to go out to spend time with the young women of Canaan.",
+    clueSource: "Genesis 34:1",
+    clueSourceContent:
+      "Now Dinah, Jacob’s daughter by Leah, used to go out to spend time with the young women of the land.",
+    choiceA: "Dinah",
+    choiceB: "Hannah",
+    choiceC: "Penninah",
+    choiceD: "Sapphira",
+    correctAnswer: "A",
   },
 ];
