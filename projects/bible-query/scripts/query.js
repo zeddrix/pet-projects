74f934cf3fe1e5f7772d5bd04afffd0f42312e67
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2020-06-15 17:12:28 +0800

    Created array for showing levels. Commented out.

--- projects/bible-query/scripts/query.js
+++ projects/bible-query/scripts/query.js
@@ -12,7 +12,7 @@ const playQueryAgain = () => {
   qIndex = 0;
   score = 0;
   const allProgressCircles = document.getElementsByClassName('progress');
-  for(let p = 0; p < allProgressCircles.length; p++) {
+  for (let p = 0; p < allProgressCircles.length; p++) {
     allProgressCircles[p].style.backgroundColor = "#FFFFFF00";
   }
 };
@@ -34,7 +34,7 @@ const renderQuestion = () => {
   const choiceC = document.getElementById("C");
   const choiceD = document.getElementById("D");
   let q = queens1[currentQuestion];
-  
+
   question.innerHTML = "<p>" + q.question + "</p>";
   clueSource.innerHTML = "<h2>" + q.clueSource + "</h2>";
   clueSourceContent.innerHTML = "<p>" + q.clueSourceContent + "</p>";
@@ -64,7 +64,7 @@ const renderCounter = () => {
   const timeGauge = document.getElementById("time-gauge");
   const counter = document.getElementById("counter");
   const questionTime = 10; // 10s
-  let gaugeWidth = 99; // em
+  let gaugeWidth = 99; // percent
   const gaugeUnit = gaugeWidth / questionTime;
 
   if (count <= questionTime) {
@@ -108,25 +108,25 @@ const scoreStarAndPercentage = () => {
     scorePercent == 100
       ? "img/three-stars.png"
       : scorePercent >= 90
-      ? "img/two-and-a-half-stars.png"
-      : scorePercent >= 70
-      ? "img/two-stars.png"
-      : scorePercent >= 50
-      ? "img/one-and-a-half-star.png"
-      : scorePercent >= 30
-      ? "img/one-star.png"
-      : scorePercent >= 15
-      ? "img/one-half-star.png"
-      : "img/zero-star.png";
-      
-    const finalScoreImgDiv = document.getElementById("final-score-img-div");
-    finalScoreImgDiv.innerHTML = "<img id='final-score-img' src=" + img + ">";
+        ? "img/two-and-a-half-stars.png"
+        : scorePercent >= 70
+          ? "img/two-stars.png"
+          : scorePercent >= 50
+            ? "img/one-and-a-half-star.png"
+            : scorePercent >= 30
+              ? "img/one-star.png"
+              : scorePercent >= 15
+                ? "img/one-half-star.png"
+                : "img/zero-star.png";
 
-    const finalScorePercentageDiv = document.getElementById(
-      "final-score-percentage-div"
-    );
-    finalScorePercentageDiv.innerHTML =
-      "<p id='final-score-percentage'>" + scorePercent + "%</p>";
+  const finalScoreImgDiv = document.getElementById("final-score-img-div");
+  finalScoreImgDiv.innerHTML = "<img id='final-score-img' src=" + img + ">";
+
+  const finalScorePercentageDiv = document.getElementById(
+    "final-score-percentage-div"
+  );
+  finalScorePercentageDiv.innerHTML =
+    "<p id='final-score-percentage'>" + scorePercent + "%</p>";
 }
 const scorePhrase1 = () => {
   const scorePercent = Math.round((100 * score) / queens1.length);
@@ -135,19 +135,19 @@ const scorePhrase1 = () => {
     scorePercent == 100
       ? "Well done!"
       : scorePercent >= 90
-      ? "Excellent!"
-      : scorePercent >= 51
-      ? "Nice!"
-      : scorePercent >= 50
-      ? "Good!"
-      : scorePercent >= 30
-      ? "Okay!"
-      : scorePercent >= 15
-      ? "Don't give up!"
-      : "Don't give up!";
+        ? "Excellent!"
+        : scorePercent >= 51
+          ? "Nice!"
+          : scorePercent >= 50
+            ? "Good!"
+            : scorePercent >= 30
+              ? "Okay!"
+              : scorePercent >= 15
+                ? "Don't give up!"
+                : "Don't give up!";
 
-    const scorePhrase1Div = document.getElementById("score-phrase-1-div");
-    scorePhrase1Div.innerHTML = "<h1 id='score-phrase-1'>" + sp1 + "</h1>";
+  const scorePhrase1Div = document.getElementById("score-phrase-1-div");
+  scorePhrase1Div.innerHTML = "<h1 id='score-phrase-1'>" + sp1 + "</h1>";
 }
 const scorePhrase2 = () => {
   const scorePercent = Math.round((100 * score) / queens1.length);
@@ -156,17 +156,17 @@ const scorePhrase2 = () => {
     scorePercent == 100
       ? "You got all the questions right!"
       : scorePercent >= 90
-      ? "You almost perfected this level!"
-      : scorePercent >= 51
-      ? "Keep it up!"
-      : scorePercent == 50
-      ? "You got half the questions right!"
-      : scorePercent >= 30
-      ? "That's okay. Play again and crush this level!"
-      : "Play again and crush this level!";
-  
-    const scorePhrase2Div = document.getElementById("score-phrase-2-div");
-    scorePhrase2Div.innerHTML = "<p id='score-phrase-2'>" + sp2 + "</p>";
+        ? "You almost perfected this level!"
+        : scorePercent >= 51
+          ? "Keep it up!"
+          : scorePercent == 50
+            ? "You got half the questions right!"
+            : scorePercent >= 30
+              ? "That's okay. Play again and crush this level!"
+              : "Play again and crush this level!";
+
+  const scorePhrase2Div = document.getElementById("score-phrase-2-div");
+  scorePhrase2Div.innerHTML = "<p id='score-phrase-2'>" + sp2 + "</p>";
 };
 const finalScore = () => {
   scorePage();
@@ -174,6 +174,14 @@ const finalScore = () => {
   scorePhrase1();
   scorePhrase2();
 };
+
+// // STARTING CODE FOR SHOWING ALL LEVELS
+// const showAllLevels = ['-------------------', 'men1', 'women1', 'men2', 'kings1', 'men3', 'queens1', 'rivers', '-------------------'];
+// showAllLevels.forEach(function (levelCategories, levelNumber) {
+//   const levelString = "level ";
+//   const showEachLevel = levelString + levelNumber + " - " + levelCategories;
+//   console.log(showEachLevel);
+// });
 
 // for (let c = 0; c < arrayLength; c++) {
 // const button = document.createElement("button");
