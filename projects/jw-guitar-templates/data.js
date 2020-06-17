64bdc337fb101449456d1fd6e4d22bade845db99
zeddrix <37854513+zeddrix@users.noreply.github.com>
2020-06-18 07:07:07 +0800

    Inputted data for ALL Kingdom Songs.

--- projects/jw-guitar-templates/data.js
+++ projects/jw-guitar-templates/data.js
@@ -1,17 +1,757 @@
 const songs = [
-  {
-    number: 78,
-    title: '"Teaching the Word of God"',
-    verse: "Acts 18:11",
-  },
-  {
-    number: 79,
-    title: "Teach Them to Stand Firm",
-    verse: "Matthew 28:19, 20",
-  },
-  {
-    number: 80,
-    title: '"Taste and See That Jehovah is Good"',
-    verse: "Psalm 34:8",
-  },
+   {
+      number: 1,
+      title: "Jehovah’s Attributes",
+      verse: "Revelation 4:11",
+   },
+   {
+      number: 2,
+      title: "Jehovah Is Your Name",
+      verse: "Psalm 83:18",
+   },
+   {
+      number: 3,
+      title: "Our Strength, Our Hope, Our Confidence",
+      verse: "Proverbs 14:26",
+   },
+   {
+      number: 4,
+      title: "“Jehovah Is My Shepherd”",
+      verse: "Psalm 23",
+   },
+   {
+      number: 5,
+      title: "God’s Wondrous Works",
+      verse: "Psalm 139",
+   },
+   {
+      number: 6,
+      title: "The Heavens Declare God’s Glory",
+      verse: "Psalm 19",
+   },
+   {
+      number: 7,
+      title: "Jehovah, Our Strength",
+      verse: "Isaiah 12:2",
+   },
+   {
+      number: 8,
+      title: "Jehovah Is Our Refuge",
+      verse: "Psalm 91",
+   },
+   {
+      number: 9,
+      title: "Jehovah Is Our King!",
+      verse: "Psalm 97:1",
+   },
+   {
+      number: 10,
+      title: "Praise Jehovah Our God!",
+      verse: "Psalm 145:12",
+   },
+   {
+      number: 11,
+      title: "Creation Praises God",
+      verse: "Psalm 19",
+   },
+   {
+      number: 12,
+      title: "Great God, Jehovah",
+      verse: "Exodus 34:6, 7",
+   },
+   {
+      number: 13,
+      title: "Christ, Our Model",
+      verse: "1 Peter 2:21",
+   },
+   {
+      number: 14,
+      title: "Praising Earth’s New King",
+      verse: "Psalm 2:12",
+   },
+   {
+      number: 15,
+      title: "Praise Jehovah’s Firstborn!",
+      verse: "Hebrews 1:6",
+   },
+   {
+      number: 16,
+      title: "Praise Jah for His Son, the Anointed",
+      verse: "Revelation 21:2",
+   },
+   {
+      number: 17,
+      title: "“I Want To”",
+      verse: "Luke 5:13",
+   },
+   {
+      number: 18,
+      title: "Grateful for the Ransom",
+      verse: "Luke 22:20",
+   },
+   {
+      number: 19,
+      title: "The Lord’s Evening Meal",
+      verse: "Matthew 26:26-30",
+   },
+   {
+      number: 20,
+      title: "You Gave Your Precious Son",
+      verse: "1 John 4:9",
+   },
+   {
+      number: 21,
+      title: "Keep On Seeking First the Kingdom",
+      verse: "Matthew 6:33",
+   },
+   {
+      number: 22,
+      title: "The Kingdom Is in Place​—Let It Come!",
+      verse: "Revelation 11:15; 12:10",
+   },
+   {
+      number: 23,
+      title: "Jehovah Begins His Rule",
+      verse: "Revelation 11:15",
+   },
+   {
+      number: 24,
+      title: "Come to Jehovah’s Mountain",
+      verse: "Isaiah 2:2-4",
+   },
+   {
+      number: 25,
+      title: "A Special Possession",
+      verse: "1 Peter 2:9",
+   },
+   {
+      number: 26,
+      title: "You Did It for Me",
+      verse: "Matthew 25:34-40",
+   },
+   {
+      number: 27,
+      title: "The Revealing of God’s Sons",
+      verse: "Romans 8:19",
+   },
+   {
+      number: 28,
+      title: "Gaining Jehovah’s Friendship",
+      verse: "Psalm 15",
+   },
+   {
+      number: 29,
+      title: "Living Up to Our Name",
+      verse: "Isaiah 43:10-12",
+   },
+   {
+      number: 30,
+      title: "My Father, My God and Friend",
+      verse: "Hebrews 6:10",
+   },
+   {
+      number: 31,
+      title: "Oh, Walk With God!",
+      verse: "Micah 6:8",
+   },
+   {
+      number: 32,
+      title: "Take Sides With Jehovah!",
+      verse: "Exodus 32:26",
+   },
+   {
+      number: 33,
+      title: "Throw Your Burden on Jehovah",
+      verse: "Psalm 55",
+   },
+   {
+      number: 34,
+      title: "Walking in Integrity",
+      verse: "Psalm 26",
+   },
+   {
+      number: 35,
+      title: "“Make Sure of the More Important Things”",
+      verse: "Philippians 1:10",
+   },
+   {
+      number: 36,
+      title: "We Guard Our Hearts",
+      verse: "Proverbs 4:23",
+   },
+   {
+      number: 37,
+      title: "Serving Jehovah Whole-Souled",
+      verse: "Matthew 22:37",
+   },
+   {
+      number: 38,
+      title: "He Will Make You Strong",
+      verse: "1 Peter 5:10",
+   },
+   {
+      number: 39,
+      title: "Make a Good Name With God",
+      verse: "Ecclesiastes 7:1",
+   },
+   {
+      number: 40,
+      title: "To Whom Do We Belong?",
+      verse: "Romans 14:8",
+   },
+   {
+      number: 41,
+      title: "Please Hear My Prayer",
+      verse: "Psalm 54",
+   },
+   {
+      number: 42,
+      title: "The Prayer of God’s Servant",
+      verse: "Ephesians 6:18",
+   },
+   {
+      number: 43,
+      title: "A Prayer of Thanks",
+      verse: "Psalm 95:2",
+   },
+   {
+      number: 44,
+      title: "A Prayer of the Lowly One",
+      verse: "Psalm 4:1",
+   },
+   {
+      number: 45,
+      title: "The Meditation of My Heart",
+      verse: "Psalm 19:14",
+   },
+   {
+      number: 46,
+      title: "We Thank You, Jehovah",
+      verse: "1 Thessalonians 5:18",
+   },
+   {
+      number: 47,
+      title: "Pray to Jehovah Each Day",
+      verse: "1 Thessalonians 5:17",
+   },
+   {
+      number: 48,
+      title: "Daily Walking With Jehovah",
+      verse: "Micah 6:8",
+   },
+   {
+      number: 49,
+      title: "Making Jehovah’s Heart Glad",
+      verse: "Proverbs 27:11",
+   },
+   {
+      number: 50,
+      title: "My Prayer of Dedication",
+      verse: "Matthew 22:37",
+   },
+   {
+      number: 51,
+      title: "To God We Are Dedicated!",
+      verse: "Matthew 16:24",
+   },
+   {
+      number: 52,
+      title: "Christian Dedication",
+      verse: "Hebrews 10:7, 9",
+   },
+   {
+      number: 53,
+      title: "Preparing to Preach",
+      verse: "Jeremiah 1:17",
+   },
+   {
+      number: 54,
+      title: "“This Is the Way”",
+      verse: "Isaiah 30:20, 21",
+   },
+   {
+      number: 55,
+      title: "Fear Them Not!",
+      verse: "Matthew 10:28",
+   },
+   {
+      number: 56,
+      title: "Make the Truth Your Own",
+      verse: "Proverbs 3:1, 2",
+   },
+   {
+      number: 57,
+      title: "Preaching to All Sorts of People",
+      verse: "1 Timothy 2:4",
+   },
+   {
+      number: 58,
+      title: "Searching for Friends of Peace",
+      verse: "Luke 10:6",
+   },
+   {
+      number: 59,
+      title: "Praise Jah With Me",
+      verse: "Psalm 146:2",
+   },
+   {
+      number: 60,
+      title: "It Means Their Life",
+      verse: "Ezekiel 3:17-19",
+   },
+   {
+      number: 61,
+      title: "Forward, You Witnesses!",
+      verse: "Luke 16:16",
+   },
+   {
+      number: 62,
+      title: "The New Song",
+      verse: "Psalm 98",
+   },
+   {
+      number: 63,
+      title: "We’re Jehovah’s Witnesses!",
+      verse: "Isaiah 43:10-12",
+   },
+   {
+      number: 64,
+      title: "Sharing Joyfully in the Harvest",
+      verse: "Matthew 13:1-23",
+   },
+   {
+      number: 65,
+      title: "Move Ahead!",
+      verse: "Hebrews 6:1",
+   },
+   {
+      number: 66,
+      title: "Declare the Good News",
+      verse: "Revelation 14:6, 7",
+   },
+   {
+      number: 67,
+      title: "“Preach the Word”",
+      verse: "2 Timothy 4:2",
+   },
+   {
+      number: 68,
+      title: "Sowing Kingdom Seed",
+      verse: "Matthew 13:4-8",
+   },
+   {
+      number: 69,
+      title: "Go Forward in Preaching the Kingdom!",
+      verse: "2 Timothy 4:5",
+   },
+   {
+      number: 70,
+      title: "Search Out Deserving Ones",
+      verse: "Matthew 10:11-15",
+   },
+   {
+      number: 71,
+      title: "We Are Jehovah’s Army!",
+      verse: "Ephesians 6:11-14",
+   },
+   {
+      number: 72,
+      title: "Making Known the Kingdom Truth",
+      verse: "Acts 20:20, 21",
+   },
+   {
+      number: 73,
+      title: "Grant Us Boldness",
+      verse: "Acts 4:29",
+   },
+   {
+      number: 74,
+      title: "Join in the Kingdom Song!",
+      verse: "Psalm 98:1",
+   },
+   {
+      number: 75,
+      title: "“Here I Am! Send Me!”",
+      verse: "Isaiah 6:8",
+   },
+   {
+      number: 76,
+      title: "How Does It Make You Feel?",
+      verse: "Hebrews 13:15",
+   },
+   {
+      number: 77,
+      title: "Light in a Darkened World",
+      verse: "2 Corinthians 4:6",
+   },
+   {
+      number: 78,
+      title: "“Teaching the Word of God”",
+      verse: "Acts 18:11",
+   },
+   {
+      number: 79,
+      title: "Teach Them to Stand Firm",
+      verse: "Matthew 28:19, 20",
+   },
+   {
+      number: 80,
+      title: "“Taste and See That Jehovah Is Good”",
+      verse: "Psalm 34:8",
+   },
+   {
+      number: 81,
+      title: "The Life of a Pioneer",
+      verse: "Ecclesiastes 11:6",
+   },
+   {
+      number: 82,
+      title: "“Let Your Light Shine”",
+      verse: "Matthew 5:16",
+   },
+   {
+      number: 83,
+      title: "“From House to House”",
+      verse: "Acts 20:20",
+   },
+   {
+      number: 84,
+      title: "Reaching Out",
+      verse: "Matthew 9:37, 38",
+   },
+   {
+      number: 85,
+      title: "Welcome One Another",
+      verse: "Romans 15:7",
+   },
+   {
+      number: 86,
+      title: "We Must Be Taught",
+      verse: "Isaiah 50:4; 54:13",
+   },
+   {
+      number: 87,
+      title: "Come! Be Refreshed",
+      verse: "Hebrews 10:24, 25",
+   },
+   {
+      number: 88,
+      title: "Make Me Know Your Ways",
+      verse: "Psalm 25:4",
+   },
+   {
+      number: 89,
+      title: "Listen, Obey, and Be Blessed",
+      verse: "Luke 11:28",
+   },
+   {
+      number: 90,
+      title: "Encourage One Another",
+      verse: "Hebrews 10:24, 25",
+   },
+   {
+      number: 91,
+      title: "Our Labor of Love",
+      verse: "Psalm 127:1",
+   },
+   {
+      number: 92,
+      title: "A Place Bearing Your Name",
+      verse: "1 Chronicles 29:16",
+   },
+   {
+      number: 93,
+      title: "Bless Our Meeting Together",
+      verse: "Hebrews 10:24, 25",
+   },
+   {
+      number: 94,
+      title: "Grateful for God’s Word",
+      verse: "Philippians 2:16",
+   },
+   {
+      number: 95,
+      title: "The Light Gets Brighter",
+      verse: "Proverbs 4:18",
+   },
+   {
+      number: 96,
+      title: "God’s Own Book​—A Treasure",
+      verse: "Proverbs 2:1",
+   },
+   {
+      number: 97,
+      title: "Life Depends on God’s Word",
+      verse: "Matthew 4:4",
+   },
+   {
+      number: 98,
+      title: "The Scriptures​—Inspired of God",
+      verse: "2 Timothy 3:16, 17",
+   },
+   {
+      number: 99,
+      title: "Myriads of Brothers",
+      verse: "Revelation 7:9, 10",
+   },
+   {
+      number: 100,
+      title: "Receive Them With Hospitality",
+      verse: "Acts 17:7",
+   },
+   {
+      number: 101,
+      title: "Working Together in Unity",
+      verse: "Ephesians 4:3",
+   },
+   {
+      number: 102,
+      title: "“Assist Those Who Are Weak”",
+      verse: "Acts 20:35",
+   },
+   {
+      number: 103,
+      title: "Shepherds​—Gifts in Men",
+      verse: "Ephesians 4:8",
+   },
+   {
+      number: 104,
+      title: "God’s Gift of Holy Spirit",
+      verse: "Luke 11:13",
+   },
+   {
+      number: 105,
+      title: "“God Is Love”",
+      verse: "1 John 4:7, 8",
+   },
+   {
+      number: 106,
+      title: "Cultivating the Quality of Love",
+      verse: "1 Corinthians 13:1-8",
+   },
+   {
+      number: 107,
+      title: "The Divine Pattern of Love",
+      verse: "1 John 4:19",
+   },
+   {
+      number: 108,
+      title: "God’s Loyal Love",
+      verse: "Isaiah 55:1-3",
+   },
+   {
+      number: 109,
+      title: "Love Intensely From the Heart",
+      verse: "1 Peter 1:22",
+   },
+   {
+      number: 110,
+      title: "“The Joy of Jehovah”",
+      verse: "Nehemiah 8:10",
+   },
+   {
+      number: 111,
+      title: "Our Reasons for Joy",
+      verse: "Matthew 5:12",
+   },
+   {
+      number: 112,
+      title: "Jehovah, God of Peace",
+      verse: "Philippians 4:9",
+   },
+   {
+      number: 113,
+      title: "Our Possession of Peace",
+      verse: "John 14:27",
+   },
+   {
+      number: 114,
+      title: "“Exercise Patience”",
+      verse: "James 5:8",
+   },
+   {
+      number: 115,
+      title: "Gratitude for Divine Patience",
+      verse: "2 Peter 3:15",
+   },
+   {
+      number: 116,
+      title: "The Power of Kindness",
+      verse: "Ephesians 4:32",
+   },
+   {
+      number: 117,
+      title: "The Quality of Goodness",
+      verse: "2 Chronicles 6:41",
+   },
+   {
+      number: 118,
+      title: "“Give Us More Faith”",
+      verse: "Luke 17:5",
+   },
+   {
+      number: 119,
+      title: "We Must Have Faith",
+      verse: "Hebrews 10:38, 39",
+   },
+   {
+      number: 120,
+      title: "Imitate Christ’s Mildness",
+      verse: "Matthew 11:28-30",
+   },
+   {
+      number: 121,
+      title: "We Need Self-Control",
+      verse: "Romans 7:14-25",
+   },
+   {
+      number: 122,
+      title: "Be Steadfast, Immovable!",
+      verse: "1 Corinthians 15:58",
+   },
+   {
+      number: 123,
+      title: "Loyally Submitting to Theocratic Order",
+      verse: "1 Corinthians 14:33",
+   },
+   {
+      number: 124,
+      title: "Ever Loyal",
+      verse: "Psalm 18:25",
+   },
+   {
+      number: 125,
+      title: "“Happy Are the Merciful!”",
+      verse: "Matthew 5:7",
+   },
+   {
+      number: 126,
+      title: "Stay Awake, Stand Firm, Grow Mighty",
+      verse: "1 Corinthians 16:13",
+   },
+   {
+      number: 127,
+      title: "The Sort of Person I Should Be",
+      verse: "2 Peter 3:11",
+   },
+   {
+      number: 128,
+      title: "Enduring to the End",
+      verse: "Matthew 24:13",
+   },
+   {
+      number: 129,
+      title: "We Will Keep Enduring",
+      verse: "Matthew 24:13",
+   },
+   {
+      number: 130,
+      title: "Be Forgiving",
+      verse: "Psalm 86:5",
+   },
+   {
+      number: 131,
+      title: "“What God Has Yoked Together”",
+      verse: "Matthew 19:5, 6",
+   },
+   {
+      number: 132,
+      title: "Now We Are One",
+      verse: "Genesis 2:23, 24",
+   },
+   {
+      number: 133,
+      title: "Worship Jehovah During Youth",
+      verse: "Ecclesiastes 12:1",
+   },
+   {
+      number: 134,
+      title: "Children Are a Trust From God",
+      verse: "Psalm 127:3-5",
+   },
+   {
+      number: 135,
+      title: "Jehovah’s Warm Appeal: “Be Wise, My Son”",
+      verse: "Proverbs 27:11",
+   },
+   {
+      number: 136,
+      title: "“A Perfect Wage” From Jehovah",
+      verse: "Ruth 2:12",
+   },
+   {
+      number: 137,
+      title: "Faithful Women, Christian Sisters",
+      verse: "Romans 16:2",
+   },
+   {
+      number: 138,
+      title: "Beauty in Gray-Headedness",
+      verse: "Proverbs 16:31",
+   },
+   {
+      number: 139,
+      title: "See Yourself When All Is New",
+      verse: "Revelation 21:1-5",
+   },
+   {
+      number: 140,
+      title: "Life Without End​—At Last!",
+      verse: "John 3:16",
+   },
+   {
+      number: 141,
+      title: "The Miracle of Life",
+      verse: "Psalm 36:9",
+   },
+   {
+      number: 142,
+      title: "Holding Fast to Our Hope",
+      verse: "Hebrews 6:18, 19",
+   },
+   {
+      number: 143,
+      title: "Keep Working, Watching, and Waiting",
+      verse: "Romans 8:20-25",
+   },
+   {
+      number: 144,
+      title: "Keep Your Eyes on the Prize!",
+      verse: "2 Corinthians 4:18",
+   },
+   {
+      number: 145,
+      title: "God’s Promise of Paradise",
+      verse: "Luke 23:43",
+   },
+   {
+      number: 146,
+      title: "“Making All Things New”",
+      verse: "Revelation 21:1-5",
+   },
+   {
+      number: 147,
+      title: "Life Everlasting Is Promised",
+      verse: "Psalm 37:29",
+   },
+   {
+      number: 148,
+      title: "Jehovah Provides Escape",
+      verse: "2 Samuel 22:1-8",
+   },
+   {
+      number: 149,
+      title: "A Victory Song",
+      verse: "Exodus 15:1",
+   },
+   {
+      number: 150,
+      title: "Seek God for Your Deliverance",
+      verse: "Zephaniah 2:3",
+   },
+   {
+      number: 151,
+      title: "He Will Call",
+      verse: "Job 14:13-15",
+   },
 ];
