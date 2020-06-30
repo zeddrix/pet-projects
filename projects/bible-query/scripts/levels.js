b59d6b407f2c70fde8124877084d443e5b97ff8d
zeddrix <37854513+zeddrix@users.noreply.github.com>
2020-06-30 11:46:50 +0800

    Created an array of objects for levels.

---
+++ projects/bible-query/scripts/levels.js
@@ -0,0 +1,38 @@
+const levels = [
+  {
+    levelNumber: 1,
+    levelCategory: "Men",
+    leveldifficulty: "basic",
+    stars: 0,
+  },
+  {
+    levelNumber: 2,
+    levelCategory: "Women",
+    leveldifficulty: "basic",
+    stars: 0,
+  },
+  {
+    levelNumber: 3,
+    levelCategory: "Men II",
+    leveldifficulty: "basic",
+    stars: 0,
+  },
+  {
+    levelNumber: 4,
+    levelCategory: "Kings",
+    leveldifficulty: "basic",
+    stars: 0,
+  },
+  {
+    levelNumber: 5,
+    levelCategory: "Men III",
+    leveldifficulty: "basic",
+    stars: 0,
+  },
+  {
+    levelNumber: 6,
+    levelCategory: "Queens",
+    leveldifficulty: "basic",
+    stars: 0,
+  },
+];
