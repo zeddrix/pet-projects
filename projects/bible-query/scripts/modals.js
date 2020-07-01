a441cd6fbeecb5d780ca6af03eda9183fdc1504e
zeddrix <37854513+zeddrix@users.noreply.github.com>
2020-07-01 20:05:06 +0800

    Added backdrop.

--- projects/bible-query/scripts/modals.js
+++ projects/bible-query/scripts/modals.js
@@ -5,36 +5,40 @@ const levelsModal = document.getElementById("levels-modal");
 const clueModal = document.getElementById("clue-modal");
 const retryQueryModal = document.getElementById("retry-query-modal");
 const quitQueryModal = document.getElementById("quit-query-modal");
+const backdrop = document.getElementById("backdrop");
 
 const showResetGameModal = () => {
   resetGameModal.style.display = "block";
+  backdrop.style.display = "block";
 };
 const showFriendsModal = () => {
   friendsModal.style.display = "block";
-};
-window.onclick = function (event) {
-  if (event.target == friendsModal) {
-    friendsModal.style.display = "none";
-  }
+  backdrop.style.display = "block";
 };
 const showQuitModal = () => {
   quitModal.style.display = "block";
+  backdrop.style.display = "block";
 };
 const showLevelsModal = () => {
   levelsModal.style.display = "block";
+  backdrop.style.display = "block";
 };
 // QUERY MODALS
 const showClueModal = () => {
   clueModal.style.display = "block";
+  backdrop.style.display = "block";
 };
 const showRetryQueryModal = () => {
   retryQueryModal.style.display = "block";
+  backdrop.style.display = "block";
 };
 const showQuitQueryModal = () => {
   quitQueryModal.style.display = "block";
+  backdrop.style.display = "block";
 };
 
 const closeModal = () => {
+  backdrop.style.display = "none";
   resetGameModal.style.display = "none";
   friendsModal.style.display = "none";
   quitModal.style.display = "none";
