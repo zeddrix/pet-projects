6bba0b517518016bd8d01a9f3cf1b78980593916
zeddrix <37854513+zeddrix@users.noreply.github.com>
2020-11-15 12:24:35 +0800

    Able to show latest repos.

--- projects/github-finder/scripts/app.js
+++ projects/github-finder/scripts/app.js
@@ -13,6 +13,7 @@ searchUser.addEventListener("keyup", (e) => {
         ui.showAlert("User not found", "alert alert-danger");
       } else {
         ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
       }
     });
   } else {
--- projects/github-finder/scripts/github.js
+++ projects/github-finder/scripts/github.js
@@ -2,6 +2,8 @@ class GitHub {
   constructor() {
     this.client_id = "f84b379b1fffbcd390d4";
     this.client_secret = "1a0c0602bccdd30d4ff4aa3d31a5461e0df06035";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
   }
 
   async getUser(user) {
@@ -9,10 +11,16 @@ class GitHub {
       `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
     );
 
+    const reposResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
     const profile = await profileResponse.json();
+    const repos = await reposResponse.json();
 
     return {
       profile,
+      repos,
     };
   }
 }
