7d2dc428f672d9942b3b0e7f6f691caa26590398
zeddrix <37854513+zeddrix@users.noreply.github.com>
2020-11-15 11:56:56 +0800

    Able to show alert when user not found.

--- projects/github-finder/scripts/app.js
+++ projects/github-finder/scripts/app.js
@@ -10,10 +10,12 @@ searchUser.addEventListener("keyup", (e) => {
     github.getUser(userText).then((data) => {
       console.log(data);
       if (data.profile.message === "Not Found") {
+        ui.showAlert("User not found", "alert alert-danger");
       } else {
         ui.showProfile(data.profile);
       }
     });
   } else {
+    ui.clearProfile();
   }
 });
