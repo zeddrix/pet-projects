3c0434dbd934b3afa1f210973bc3136a5f80f1d3
zeddrix <37854513+zeddrix@users.noreply.github.com>
2020-11-17 12:05:30 +0800

    Able to log posts on console.

--- projects/microposts/original/src/app.js
+++ projects/microposts/original/src/app.js
@@ -1,10 +1,22 @@
-const greeting = 'Hello World';
-console.log(greeting);
+// const greeting = 'Hello World';
+// console.log(greeting);
 
-const getData = async (url) => {
-  const response = await fetch(url);
-  const result = await response.json();
-  console.log(result);
+// const getData = async (url) => {
+//   const response = await fetch(url);
+//   const result = await response.json();
+//   console.log(result);
+// };
+
+// getData('https://jsonplaceholder.typicode.com/posts');
+
+import { http } from "./http";
+import { ui } from "./ui";
+
+const getPosts = () => {
+  http
+    .get("http://localhost:3000/posts")
+    .then((data) => ui.showPosts(data))
+    .catch((err) => console.log(err));
 };
 
-getData('https://jsonplaceholder.typicode.com/posts');
+document.addEventListener("DOMContentLoaded", getPosts);
