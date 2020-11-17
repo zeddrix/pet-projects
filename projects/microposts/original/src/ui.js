54fbbbc541ea304bd3eab524491197f41b07a1c6
zeddrix <37854513+zeddrix@users.noreply.github.com>
2020-11-17 12:20:13 +0800

    Added edit and delete icons.

--- projects/microposts/original/src/ui.js
+++ projects/microposts/original/src/ui.js
@@ -17,6 +17,12 @@ class UI {
          <div class="card-body">
             <h4 class="card-title">${post.title}</h4>
             <p class="card-text">${post.body}</p>
+            <a href="#" class="edit card-link" data-id="${post.id}">
+               <i class="fa fa-pencil"></i>
+            </a>
+            <a href="#" class="delete card-link" data-id="${post.id}">
+               <i class="fa fa-remove"></i>
+            </a>
          </div>
       </div>
        `;
