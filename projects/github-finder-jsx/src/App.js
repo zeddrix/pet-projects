9bb4f2cd1a63c580b5c518591d58d177789b5796
zeddrix <37854513+zeddrix@users.noreply.github.com>
2020-11-24 18:10:17 +0800

    Show alert when input field's empty.

--- projects/github-finder-jsx/src/App.js
+++ projects/github-finder-jsx/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
 import './App.css';
 
@@ -9,6 +10,7 @@ class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null,
   };
 
   // NOTE: Uncomment this function to see some users on initial page
@@ -34,6 +36,12 @@ class App extends Component {
 
   clearUsers = () => this.setState({ users: [], loading: false });
 
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+
+    setTimeout(() => this.setState({ alert: null }), 1500);
+  };
+
   render() {
     const { users, loading } = this.state;
 
@@ -41,10 +49,12 @@ class App extends Component {
       <div className='App'>
         <Navbar />
         <div className='container'>
+          <Alert alert={this.state.alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={loading} users={users} />
         </div>
