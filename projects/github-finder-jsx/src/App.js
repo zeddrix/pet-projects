4449a1c0ba8fbcb0454841b4bb4b852845a3aaad
zeddrix <37854513+zeddrix@users.noreply.github.com>
2020-11-24 17:58:41 +0800

    Refactored code.

--- projects/github-finder-jsx/src/App.js
+++ projects/github-finder-jsx/src/App.js
@@ -35,6 +35,8 @@ class App extends Component {
   clearUsers = () => this.setState({ users: [], loading: false });
 
   render() {
+    const { users, loading } = this.state;
+
     return (
       <div className='App'>
         <Navbar />
@@ -42,9 +44,9 @@ class App extends Component {
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
-            showClear={this.state.users.length > 0 ? true : false}
+            showClear={users.length > 0 ? true : false}
           />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
