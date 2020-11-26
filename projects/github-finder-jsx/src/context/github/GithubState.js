dd153f3112fb73d5680916a25db202ddd26ad616
zeddrix <37854513+zeddrix@users.noreply.github.com>
2020-11-26 15:49:37 +0800

    Implemented Context.

---
+++ projects/github-finder-jsx/src/context/github/GithubState.js
@@ -0,0 +1,39 @@
+import React, { useReducer } from 'react';
+import axios from 'axios';
+import GithubContext from './githubContext';
+import GithubReducer from './githubReducer';
+import {
+  SEARCH_USERS,
+  GET_USER,
+  CLEAR_USERS,
+  GET_REPOS,
+  SET_LOADING,
+  SET_ALERT,
+  REMOVE_ALERT,
+} from '../types';
+
+const GithubState = (props) => {
+  const initialState = {
+    users: [],
+    user: {},
+    repos: [],
+    loading: false,
+  };
+
+  const [state, dispatch] = useReducer(GithubReducer, initialState);
+
+  return (
+    <GithubContext.Provider
+      value={{
+        users: state.users,
+        user: state.user,
+        repos: state.repos,
+        loading: state.loading,
+      }}
+    >
+      {props.children}
+    </GithubContext.Provider>
+  );
+};
+
+export default GithubState;
