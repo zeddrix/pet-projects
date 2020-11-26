84c9f1eacc7d7e37daaa95a41e2f4e322b2cb8fa
zeddrix <37854513+zeddrix@users.noreply.github.com>
2020-11-26 15:16:08 +0800

    Converted to func component with useState.

--- projects/github-finder-jsx/src/components/users/User.js
+++ projects/github-finder-jsx/src/components/users/User.js
@@ -58,7 +58,7 @@ const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
               <p>{bio}</p>
             </Fragment>
           )}
-          <a href={html_url} className='btn btn-dark my-1'>
+          <a href={html_url} target='_blank_' className='btn btn-dark my-1'>
             Visit GitHub Profile
           </a>
           <ul>
