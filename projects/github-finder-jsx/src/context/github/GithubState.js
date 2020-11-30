adfb3000a505e38090b5b63d3561b5f6b9669cce
zeddrix <37854513+zeddrix@users.noreply.github.com>
2020-11-30 15:47:20 +0800

    Moved Repos state to Context.

--- projects/github-finder-jsx/src/context/github/GithubState.js
+++ projects/github-finder-jsx/src/context/github/GithubState.js
@@ -44,6 +44,17 @@ const GithubState = (props) => {
     });
   };
 
+  const getUserRepos = async (username) => {
+    setLoading();
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    dispatch({
+      type: GET_REPOS,
+      payload: res.data,
+    });
+  };
+
   const clearUsers = () => dispatch({ type: CLEAR_USERS });
 
   const setLoading = () => dispatch({ type: SET_LOADING });
@@ -58,6 +69,7 @@ const GithubState = (props) => {
         searchUsers,
         clearUsers,
         getUser,
+        getUserRepos,
       }}
     >
       {props.children}
