4ecffd0d2ff860c40bf345fcb4480284c475ab5d
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2021-03-05 13:28:46 +0800

    Applied GeoJSON location & Geocoder Hook.

--- projects/devcamper-api/server.js
+++ projects/devcamper-api/server.js
@@ -34,7 +34,7 @@ const PORT = process.env.PORT || 5000;
 const server = app.listen(
 	PORT,
 	console.log(
-		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.blue.bold
 	)
 );
 
