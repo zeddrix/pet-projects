903f97aeed20638708d9ebe16d9d88548341c475
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2021-03-05 13:14:28 +0800

    Used slugify to generate slug for bootcamp.

--- projects/devcamper-api/models/Bootcamp.js
+++ projects/devcamper-api/models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const BootcampSchema = new mongoose.Schema({
 	name: {
@@ -96,6 +97,13 @@ const BootcampSchema = new mongoose.Schema({
 		type: Date,
 		default: Date.now,
 	},
+});
+
+// Create bootcamp slug from the name
+BootcampSchema.pre('save', function (next) {
+	this.slug = slugify(this.name, { lower: true });
+	console.log(`Slugify ran for ${this.name}. Slug: ${this.slug}`);
+	next();
 });
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
