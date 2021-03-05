0f4bc0244a8abab17aa996b7db64666fcded91ee
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2021-03-05 11:48:39 +0800

    Can update bootcamp.

--- projects/devcamper-api/controllers/bootcamps.js
+++ projects/devcamper-api/controllers/bootcamps.js
@@ -49,10 +49,17 @@ exports.createBootcamp = async (req, res, next) => {
 // @desc      Update bootcamp
 // @route     PUT /api/v1/bootcamps/:id
 // @access    Private
-exports.updateBootcamp = (req, res, next) => {
-	res
-		.status(200)
-		.json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+exports.updateBootcamp = async (req, res, next) => {
+	const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+		runValidators: true,
+	});
+
+	if (!bootcamp) {
+		return res.status(400).json({ success: false });
+	}
+
+	res.status(200).json({ success: true, data: bootcamp });
 };
 
 // @desc      Delete bootcamp
