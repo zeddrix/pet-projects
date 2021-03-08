ad5c18b77bf1707d2bca3095b06ff588211d9c4a
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2021-03-08 16:49:15 +0800

    Able to sort bootcamps.

--- projects/devcamper-api/controllers/bootcamps.js
+++ projects/devcamper-api/controllers/bootcamps.js
@@ -14,7 +14,7 @@ exports.getBootcamps = asyncHandler(
 		const reqQuery = { ...req.query };
 
 		// Fields to exclude
-		const removeFields = ['select'];
+		const removeFields = ['select', 'sort'];
 
 		// Loop over removeFields and delete them from reqQuery
 		removeFields.forEach((param) => delete reqQuery[param]);
@@ -36,6 +36,14 @@ exports.getBootcamps = asyncHandler(
 		if (req.query.select) {
 			const fields = req.query.select.split(',').join(' ');
 			query = query.select(fields);
+		}
+
+		// Sort
+		if (req.query.sort) {
+			const sortBy = req.query.sort.split(',').join(' ');
+			query = query.sort(sortBy);
+		} else {
+			query = query.sort('-createdAt');
 		}
 
 		// Execute query
