95a038aa144eec8f9c654d1e500292aa46db2ed9
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2021-03-12 11:30:16 +0800

    Only owner of bootcamp can add new course.

--- projects/devcamper-api/controllers/courses.js
+++ projects/devcamper-api/controllers/courses.js
@@ -48,6 +48,7 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
 // @access    Private
 exports.addCourse = asyncHandler(async (req, res, next) => {
 	req.body.bootcamp = req.params.bootcampId;
+	req.body.user = req.user.id;
 
 	const bootcamp = await Bootcamp.findById(req.params.bootcampId);
 
@@ -57,6 +58,16 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
 				`No bootcamp found with the id of ${req.params.bootcampId}`
 			),
 			404
+		);
+	}
+
+	// Make sure user is bootcamp owner
+	if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+		return next(
+			new ErrorResponse(
+				`User ${req.user.id} is not authorized to add a course to bootcamp ${bootcamp._id}`,
+				401
+			)
 		);
 	}
 
