80d016e0bbc515d32819ec38a41b26f9ddf2249b
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2021-03-15 16:19:06 +0800

    Update logged in user password.

--- projects/devcamper-api/routes/auth.js
+++ projects/devcamper-api/routes/auth.js
@@ -6,6 +6,7 @@ const {
 	forgotPassword,
 	resetPassword,
 	updateDetails,
+	updatePassword,
 } = require('../controllers/auth');
 
 const router = express.Router();
@@ -16,6 +17,7 @@ router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
 router.put('/updatedetails', protect, updateDetails);
+router.put('/updatepassword', protect, updatePassword);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
 
