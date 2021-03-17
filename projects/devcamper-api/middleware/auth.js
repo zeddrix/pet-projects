16883c03e6f2752983b05549f0e3a2bdf708b979
Zeddrix Fabian <zeddrix.fabian@gmail.com>
2021-03-17 18:03:22 +0800

    Clear token cookie to log user out.

--- projects/devcamper-api/middleware/auth.js
+++ projects/devcamper-api/middleware/auth.js
@@ -11,7 +11,9 @@ exports.protect = asyncHandler(async (req, res, next) => {
 		req.headers.authorization &&
 		req.headers.authorization.startsWith('Bearer')
 	) {
+		// Set token from Bearer Token in header
 		token = req.headers.authorization.split(' ')[1];
+		// Set token from cookie
 	} else if (req.cookies.token) {
 		token = req.cookies.token;
 	}
